4b23cf0f8571e927555c49db942e2af19cd91a72
danielvito-pereira <danielvitor081@gmail.com>
2024-01-07 07:52:08 -0300

    manipulando o resultado

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,6 +2,7 @@ const offset = 0;
 const limit = 10;
 const url = `http://pokeapi.co/api/v2/pokemon?off=${offset}&limit=${limit}`
 
-fetch(url).then((response) =>{
-  
-})
+fetch(url)
+  .then((response) => response.json())
+  .then((jsonBody) => console.log(jsonBody))
+  .catch((error) => console.error(error))
