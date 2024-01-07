9dad5a86ab0e49b4abd44a89ef20e61926cb40d9
danielvito-pereira <danielvitor081@gmail.com>
2024-01-07 10:48:41 -0300

    separando o consumo da api da manipulação

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,39 @@
 const offset = 0;
 const limit = 10;
-const url = `http://pokeapi.co/api/v2/pokemon?off=${offset}&limit=${limit}`
 
-fetch(url)
-  .then((response) => response.json())
-  .then((jsonBody) => console.log(jsonBody))
-  .catch((error) => console.error(error))
+
+
+function  convertPokemonToLi(pokemon){
+  return`
+    <li class="pokemon">
+      <div class="info">
+        <span class="name">${pokemon.name}</span>
+        <span class="number">#001</span>
+      </div>
+      <div class="detail">
+        <ol class="types">
+          <li class="type">gras</li>
+          <li class="type">poison</li>
+        </ol>
+        <img src="" alt="${pokemon.name}" srcset="https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png">
+      </div>
+    </li>
+  `
+}
+
+const pokemonList = document.getElementById('pokemonList')
+
+pokeApi.getPokemons().then((pokemons) => {
+  const ListItems =[]
+
+
+  
+    for(let i = 0; i < pokemons.length; i++){
+      const pokemon = pokemons[i];
+      ListItems.push(convertPokemonToLi(pokemon))
+    }
+
+  })
+
+
+  
